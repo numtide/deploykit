8b3c19d161fec62331323e6f3ec35927b967828f
Jörg Thalheim <joerg@thalheim.io>
2022-08-18 15:16:21 +0200

    tests/sshd: also generate ssh key

--- tests/sshd.py
+++ tests/sshd.py
@@ -1,7 +1,8 @@
 import pytest
 import shutil
 import subprocess
-from tempfile import NamedTemporaryFile
+from pathlib import Path
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import Iterator
 
 from ports import Ports
@@ -17,17 +18,33 @@ class Sshd:
 @pytest.fixture
 def sshd(command: Command, ports: Ports) -> Iterator[Sshd]:
     """ """
-    with NamedTemporaryFile(mode="w") as f:
-        f.write("HostKey /tmp/id_ed25519")
-        f.flush()
+    with TemporaryDirectory() as _dir:
+        dir = Path(_dir)
+        host_key = dir / "host_ssh_host_ed25519_key"
+        proc = command.run(
+            [
+                "ssh-keygen",
+                "-t",
+                "ed25519",
+                "-f",
+                host_key,
+                "-N",
+                "",
+            ]
+        )
+        proc.wait()
+        sshd_config = dir / "sshd_config"
+        sshd_config.write_text(f"HostKey {host_key}")
+
         port = ports.allocate(1)
         sshd = shutil.which("sshd")
         proc = command.run(
-            [sshd, "-f", f.name, "-D", "-d", "-p", str(port), "-6"],
-            stdout=subprocess.PIPE,
+            [sshd, "-f", str(sshd_config), "-D", "-d", "-p", str(port), "-6"],
+            stderr=subprocess.PIPE,
         )
-        assert proc.stdout is not None
-        for line in proc.stdout:
+        assert proc.stderr is not None
+        for line in proc.stderr:
             if "Server listening on ::" in line:
                 yield Sshd(port, proc)
+            print(line, end="")
         raise RuntimeError("Could not start sshd")
