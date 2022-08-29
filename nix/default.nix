726fbc216d3235e5bb77bf6c71cb1e50dbf62f05
Jörg Thalheim <joerg@thalheim.io>
2022-08-29 10:44:20 +0200

    share sshd config between test runs

--- nix/default.nix
+++ nix/default.nix
@@ -6,6 +6,8 @@
 , pytest
 , glibcLocales
 , pytestCheckHook
+, openssh
+, lib
 }:
 
 buildPythonPackage rec {
@@ -18,7 +20,13 @@ buildPythonPackage rec {
     flake8
     glibcLocales
     pytestCheckHook
+    openssh
   ];
+
+  #preCheck = ''echo "sleep ...."; sleep 99999'';
+
+  # don't swallow stdout/stderr
+  pytestFlagsArray = [ "-s" ];
 
   postCheck = ''
     echo -e "\x1b[32m## run black\x1b[0m"
@@ -28,4 +36,11 @@ buildPythonPackage rec {
     echo -e "\x1b[32m## run mypy\x1b[0m"
     mypy --strict nixpkgs_review
   '';
+  meta = with lib; {
+    description = "Execute commands remote via ssh and locally in parallel with python";
+    homepage = "https://github.com/numtide/deploykit";
+    license = licenses.mit;
+    maintainers = with maintainers; [ mic92 ];
+    platforms = platforms.unix;
+  };
 }
--- tests/sshd.py
+++ tests/sshd.py
@@ -17,14 +17,19 @@ class Sshd:
         self.key = key
 
 
-@pytest.fixture
-def sshd(command: Command, ports: Ports, project_root: Path) -> Iterator[Sshd]:
-    """ """
+class SshdConfig:
+    def __init__(self, path: str, key: str) -> None:
+        self.path = path
+        self.key = key
+
+
+@pytest.fixture(scope="session")
+def sshd_config(project_root: Path) -> Iterator[SshdConfig]:
     # FIXME, if any parent of `project_root` is world-writable than sshd will refuse it.
     with TemporaryDirectory(dir=project_root) as _dir:
         dir = Path(_dir)
         host_key = dir / "host_ssh_host_ed25519_key"
-        proc = command.run(
+        subprocess.run(
             [
                 "ssh-keygen",
                 "-t",
@@ -33,28 +38,34 @@ def sshd(command: Command, ports: Ports, project_root: Path) -> Iterator[Sshd]:
                 host_key,
                 "-N",
                 "",
-            ]
+            ],
+            check=True,
         )
-        proc.wait()
 
         sshd_config = dir / "sshd_config"
         sshd_config.write_text(
             f"""
         HostKey {host_key}
+        LogLevel DEBUG3
         AuthorizedKeysFile {host_key}.pub
         """
         )
+        yield SshdConfig(str(sshd_config), str(host_key))
 
-        port = ports.allocate(1)
-        sshd = shutil.which("sshd")
-        proc = command.run([sshd, "-f", str(sshd_config), "-D", "-p", str(port), "-6"])
 
-        while True:
-            if check_port(port):
-                yield Sshd(port, proc, str(host_key))
-                return
-            else:
-                rc = proc.poll()
-                if rc is not None:
-                    raise Exception(f"sshd processes was terminated with {rc}")
-                time.sleep(0.1)
+@pytest.fixture
+def sshd(sshd_config: SshdConfig, command: Command, ports: Ports) -> Iterator[Sshd]:
+    port = ports.allocate(1)
+    sshd = shutil.which("sshd")
+    assert sshd is not None, "no sshd binary found"
+    proc = command.run([sshd, "-f", sshd_config.path, "-D", "-p", str(port)])
+
+    while True:
+        if check_port(port):
+            yield Sshd(port, proc, sshd_config.key)
+            return
+        else:
+            rc = proc.poll()
+            if rc is not None:
+                raise Exception(f"sshd processes was terminated with {rc}")
+            time.sleep(0.1)
